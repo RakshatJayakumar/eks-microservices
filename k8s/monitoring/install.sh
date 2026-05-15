1bfa1b82e0c76f29ce8f9dc0eeda6903af29bd0e
RakshatJayakumar <x22245766@student.ncirl.ie>
2026-05-15 21:26:20 +0100

    feat: add Prometheus and Grafana monitoring stack via Helm

---
+++ k8s/monitoring/install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Deploy Prometheus + Grafana monitoring stack on EKS
+# Run after: aws eks update-kubeconfig --name <cluster-name> --region <region>
+
+set -e
+
+echo "Adding Helm repositories..."
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+echo "Creating monitoring namespace..."
+kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Installing Prometheus..."
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \
+  --namespace monitoring \
+  --values prometheus-values.yaml \
+  --wait
+
+echo "Installing Grafana..."
+helm upgrade --install grafana grafana/grafana \
+  --namespace monitoring \
+  --values grafana-values.yaml \
+  --wait
+
+echo ""
+echo "Monitoring stack deployed successfully!"
+echo ""
+echo "Get Grafana URL:"
+echo "kubectl get svc grafana -n monitoring"
+echo ""
+echo "Default credentials: admin / admin123"
+echo ""
+echo "Useful commands:"
+echo "  kubectl get pods -n monitoring"
+echo "  kubectl get svc -n monitoring"
